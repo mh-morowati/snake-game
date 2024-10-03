d750473071d260f4e11323c3cf9a3f6446bc6362
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-10-03 14:48:35 +0330

    .

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -4,18 +4,17 @@ interface FoodProps {
   food: { x: number, y: number };
 }
 
- const Food: React.FC<FoodProps> = ({ food }) => {
+const Food: React.FC<FoodProps> = ({ food }) => {
   return (
     <div
       className="absolute bg-red-500 w-30 h-30"
       style={{
-      
+
         left: `${food.x * 30}px`,
         top: `${food.y * 30}px`
-      }}
-    />
+      }}></div>
   );
 };
 
- 
+
 export default Food;
